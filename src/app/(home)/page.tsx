6f42aaf8cad404216efced26140c1dec86ed58a3
Dennise Catolos <dentolos19@gmail.com>
2024-05-25 12:45:42 +0800

    Added initial project files

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import KeyboardVoiceIcon from "@mui/icons-material/KeyboardVoice";
+import { IconButton } from "@mui/material";
+
+export default function Page() {
+	return (
+		<main>
+			<h1>Hello, world!</h1>
+			<IconButton>
+				<KeyboardVoiceIcon />
+			</IconButton>
+		</main>
+	);
+}
